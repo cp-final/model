e1b2a6d88edf92a5da3d4ef8c26891a8fdc5d2fc
kirkylesich <stealer-mail@mail.ru>
2021-12-03 19:57:07 +0500

    fix

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import numpy as np
 import pandas as pd
+from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import roc_auc_score
 from datetime import *
 import fasttext 
 import fasttext.util
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from multiprocessing.pool import ThreadPool
+import pickle
 
 
-df = pd.read_csv('200k_train.csv')[:50]
+df = pd.read_csv('200k_train.csv')
+apps_df = pd.read_csv('apps_id.csv')
 print("Reading CSV")
 
 
@@ -22,6 +26,14 @@ cat_cols = [
     'gamecategory',
     'subgamecategory'
 ]
+    
+def get_ios_app(app_id):
+    return apps_df[apps_df['id'] == app_id]['app']
+
+def normalize_ios(df):
+     
+    pass
+
 
 def convCol(df1, name):
   le = LabelEncoder()
@@ -51,7 +63,13 @@ def clear_csv(df):
     for i, item in enumerate(df['created']):
       time = datetime.strptime(item, "%Y-%m-%d %H:%M:%S")
       df.loc[i, 'created'] = int(time.hour)
+      if df['os'][i] == "ios":
+          app_name = get_ios_app(df['bundle'][i])
+          print(app_name)
+          if not app_name.empty:
+              df.loc[i, 'bundle'] = str(app_name)
 
+    
 
 
     print("Stage 3 encoding")
@@ -67,16 +85,26 @@ dfg = clear_csv(df)
 print("End clearing CSV")
 
 
+
 ft_model = fasttext.load_model('cc.en.300.bin')
+fasttext.util.reduce_model(ft_model, 30)
 
 def process(dff, model):
-  data = []
-  for i, item in enumerate(dff['bundle']):
-      if type(item) == str:
-          vector_word = model.get_word_vector(str(item).lower())
-          data.append(vector_word)
-      else:
-          data.append(np.nan)
+    data = []
+    for i, item in enumerate(dff['bundle']):
+        if type(item) != float:
+            if not item.isnumeric():
+                if type(item) == str:
+                    vector_word = model.get_word_vector(str(item).lower())
+                    data.append(vector_word)
+                else:
+                    data.append(np.nan)
+            else:
+                vector_word = model.get_word_vector(str(item).lower())
+                data.append(vector_word)
+        else:
+            data.append(np.nan)
+
   dff['bundle'] = data
   return dff
 
@@ -89,15 +117,8 @@ vectorized_df = process(dfg, ft_model)
 vectorized_df = vectorized_df.drop('osv', axis=1)
 vectorized_df = vectorized_df.dropna()
 
+
 print("Dataframe vectorized")
-
-
-#for i, item in enumerate(fdf['bundle']):
-#  if type(item) !=float:
-#    fdf['bundle'][i] = np.fromstring(item[1:len(item)-1], dtype=float, sep=' ')
-#  else: 
-#    fdf['bundle'][i] = np.array[np.NaN]
-#fdf.info()
 
 def create_new_column(df):
   fg = pd.DataFrame(np.zeros((len(df), 30)))
@@ -114,13 +135,12 @@ predata = predata.drop('bundle', axis=1)
 predata = predata.dropna()
 
 predata.to_csv('predata2.csv', index=False)
+print(predata)
 print("Saved predata")
 
 y = predata['Segment']
 X = predata.drop(['Segment'], axis=1)
 
-copy_X = X
-X['created'] = vectorized_df['created']
 ans = pd.concat([X, y], axis=1)
 ans = ans.dropna()
 
@@ -129,74 +149,25 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random
 
 is_NaN = ans.isnull()
 row_has_NaN = is_NaN.any(axis=1)
-rows_with_NaN = vectorized_df[row_has_NaN]
+#rows_with_NaN = vectorized_df[row_has_NaN]
 
 new_dataframe = ans.replace([np.inf, -np.inf], np.nan).dropna(axis=0)
 
 Y = new_dataframe['Segment']
 X = new_dataframe.drop('Segment', axis=1)
 
+
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.10, random_state=42)
 
 print("Start learning NN")
-kmeans = KMeans(n_clusters=5, random_state=58, max_iter=10000, algorithm='elkan')
-print(kmeans.fit_predict(X_train, y_train))
+clf = MLPClassifier(random_state=1, max_iter=300, verbose=1, solver = 'adam', learning_rate='adaptive', hidden_layer_sizes=(120), activation='logistic', learning_rate_init=0.001).fit(X_train, y_train)
 
-#ans_model_kmean = kmeans.predict(X_test)
-#print(ans_model_kmean)
-#
-#print(y_test)
-#
-#
-#
-#print(len(y_test))
-#y_test_np = y_test.to_numpy()
-#print(type(y_test_np))
-#print(len(ans_model_kmean))
-#print(type(ans_model_kmean))
-#
-#score = 0
-#for i in range(len(ans_model_kmean)):
-#  if y_test_np[i] == ans_model_kmean[i]:
-#    score+=1
-#print(score/len(ans_model_kmean))
-#
-##К средних не отвечает сложности задачи.
-#
-#from sklearn.neural_network import MLPClassifier
-#
-#clf = MLPClassifier(random_state=1, max_iter=300, verbose=1, solver = 'adam', learning_rate='adaptive', hidden_layer_sizes=(120), activation='tanh').fit(X_train, y_train)
-#
-#predict_clf = clf.predict(X_test)
-#
-#print(len(y_test))
-#y_test_np = y_test.to_numpy()
-#print(type(y_test_np))
-#print(len(predict_clf))
-#print(type(predict_clf))
-#
-#predict_clf
-#
-#score = 0
-#for i in range(len(predict_clf)):
-#  if y_test_np[i] == predict_clf[i]:
-#    score+=1
-#print(score/len(predict_clf))
-#
-#import pickle
-#
-#filename = '/content/drive/MyDrive/dataset/model0.64.mdl'
-#pickle.dump(clf, open(filename, 'wb'))
-#
-#from sklearn.metrics import roc_auc_score
-#
-#y_test = y_test.to_numpy()
-#
-#type(y_test)
-#
-#type(predict_clf)
-#
-#roc_auc_score(y_test, clf.predict_proba(X_test), multi_class='ovr')
-#
-#roc_auc_score(y_train, clf.predict_proba(X_train), multi_class='ovr')
-#
+filename = 'model0.64.mdl'
+pickle.dump(clf, open(filename, 'wb'))
+
+y_test_np = y_test.to_numpy()
+x_test_np = X_test
+print(y_test_np.shape)
+print(clf.predict_proba(x_test_np).shape)
+print(roc_auc_score(y_test_np, clf.predict(X_test), multi_class='ovr'))
+
